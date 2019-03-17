f611ca0d2830af233f9d01273e2c12997e0c5416
banavijaya <banavijaya@users.noreply.github.com>
2019-03-17 12:44:28 +0530

    initial commit

---
+++ src/test/java/com/example/myFirstRest/MyFirstRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.myFirstRest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyFirstRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
